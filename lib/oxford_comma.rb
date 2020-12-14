1e1e9a96ea3e31ca7fc3fb9fd2cbd66665981400
Jonathan Montesdeoca <JMontesdeoca93@gmail.com>
2020-12-14 09:42:20 -0500

    commit tree 10c4495af08b5dba7b22bfdce7a1da7804407880

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+    if array.count == 1
+        return  "kiwi"
+            elsif array.count ==2
+                string = array.join(" and ")
+                string
+            else
+                string = array[0..-2].join(", ")
+                string << ", and"
+                string << " #{array[-1]}"
+                string
+            end
 
 end
